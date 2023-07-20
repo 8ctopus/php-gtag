f6b3235bea1e9da7d48c05a7a69ae0223c7038e4
8ctopus <hello@octopuslabs.io>
2023-07-20 11:59:18 +0400

    Cleanup demo code

--- demo.php
+++ demo.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Oct8pus\Gtag\Event;
 use Oct8pus\Gtag\Gtag;
-use Oct8pus\Gtag\Payload;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -35,31 +34,3 @@ $event = new Event([
 
 $gtag->send($event);
 exit;
-
-$random = strtolower(bin2hex(random_bytes(2)));
-
-$event = new Event([
-    'event_name' => 'page_view',
-    'document_location' => "http://test.com/{$random}/",
-    'document_referrer' => 'http://test.com/',
-    'document_title' => $random,
-]);
-
-$gtag->send($event);
-
-echo "enter payload:\n";
-
-$source = stream_get_line(STDIN, 1024, PHP_EOL . PHP_EOL);
-
-$source = str_replace("\r", '', $source);
-
-$converted = (new Payload())
-    ->analyze($source);
-
-echo "enter file name: ";
-$filename = trim(fgets(STDIN));
-
-$file = __DIR__ . "/events/{$filename}.event";
-
-file_put_contents($file, $converted . "\n\noriginal payload\n" . $source . "\n");
-
