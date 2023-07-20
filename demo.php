9ef07e71eef8debcea212d8d658bfdd9bc5b69df
8ctopus <hello@octopuslabs.io>
2023-07-20 12:12:19 +0400

    Add purchase event (wip) Sort properties

--- demo.php
+++ demo.php
@@ -20,6 +20,7 @@ $gtag->addParams([
     'screen_resolution' => '1920x1080',
 ]);
 
+/*
 $random = strtolower(bin2hex(random_bytes(2)));
 
 $page = 'gtag-index.php';
@@ -27,10 +28,38 @@ $title = 'My First Web Page';
 
 $event = new Event([
     'event_name' => 'page_view',
+
     'document_location' => "http://test.com/{$page}",
     'document_referrer' => 'http://test.com/',
     'document_title' => $title,
 ]);
+*/
+
+$event = new Event([
+    'event_name' => 'page_view',
+
+    'document_location' => "http://test.com/gtag-purchase.php",
+    'document_referrer' => 'http://test.com/gtag-index.php',
+    'document_title' => '',
+]);
 
 $gtag->send($event);
+
+$event = new Event([
+    'event_name' => 'purchase',
+
+    'document_location' => "http://test.com/gtag-purchase.php",
+    'document_referrer' => 'http://test.com/gtag-index.php',
+    'document_title' => '',
+
+    'conversion' => true,
+    'transaction_id' => strtoupper(bin2hex(random_bytes(2))),
+    'currency' => 'USD',
+    'transaction_value' => 10,
+    'product_1' => 'nmpaper~qt1~pr5',
+    'product_2' => 'nmpencil~qt1~pr5',
+]);
+
+$gtag->send($event);
+
 exit;
