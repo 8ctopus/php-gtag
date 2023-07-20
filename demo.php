3ea3aade3855c638850544cc08f95eada2defd62
8ctopus <hello@octopuslabs.io>
2023-07-20 16:16:55 +0400

    Update session cookie

--- demo.php
+++ demo.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $gtag = new Gtag([
     '_ga' => 'GA1.1.1827526090.1689745728',
     // expired session
-    '_ga_8XQMZ2E6TH' => 'GS1.1.1689832664.9.1.1689833933.0.0.0',
+    '_ga_8XQMZ2E6TH' => 'GS1.1.1689852319.10.1.1689852323.0.0.0',
     // valid session
     //'_ga_8XQMZ2E6TH' => 'GS1.1.1689838164.6.1.1689840046.0.0.0',
 ], true);
