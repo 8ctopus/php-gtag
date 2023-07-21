51d59632b3690922f13330f27107237c39379f93
8ctopus <hello@octopuslabs.io>
2023-07-21 10:28:45 +0400

    Add purchase event basic test

---
+++ tests/PurchaseEventTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Oct8pus\Gtag\PurchaseEvent;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @covers \Oct8pus\Gtag\PurchaseEvent
+ * @covers \Oct8pus\Gtag\AbstractEvent
+ */
+final class PurchaseEventTest extends TestCase
+{
+    public function test() : void
+    {
+        $event = new PurchaseEvent();
+
+        $event
+            ->setDocumentLocation('http://test.com/purchase.php')
+            ->setDocumentReferrer('http://test.com/')
+            ->setDocumentTitle('Purchase')
+            ->setTransactionId('T-112')
+            ->setTransactionValue(16.97)
+            ->setCurrency('USD')
+            ->addItem('pen', 2, 5.99)
+            ->addItem('paper and cisors', 1, 4.99);
+
+        $event->valid();
+
+        static::assertSame($event->name(), 'purchase');
+
+        $expected = [
+            'cu' => 'USD',
+            'dl' => 'http://test.com/purchase.php',
+            'dr' => 'http://test.com/',
+            'dt' => 'Purchase',
+            'en' => 'purchase',
+            '_c' => true,
+            'pr1' => 'nmpen~pr5.99~qt2',
+            'pr2' => 'nmpaper and cisors~pr4.99~qt1',
+            'ep.transaction_id' => 'T-112',
+            'epn.value' => 16.97,
+        ];
+
+        static::assertSame($expected, $event->encode([]));
+
+        $expected = <<<TEXT
+        cu: USD
+        dl: http://test.com/purchase.php
+        dr: http://test.com/
+        dt: Purchase
+        en: purchase
+        _c: 1
+        pr1: nmpen~pr5.99~qt2
+        pr2: nmpaper and cisors~pr4.99~qt1
+        ep.transaction_id: T-112
+        epn.value: 16.97
+
+        TEXT;
+
+        static::assertSame($expected, $event->ini([]));
+    }
+}
